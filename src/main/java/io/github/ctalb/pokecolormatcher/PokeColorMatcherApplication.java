1067121ed4d65aae40cf254b6e3a00874819e9bb
ctalb39 <ctalb39@wgu.edu>
2025-06-03 17:31:32 -0400

    Initial commit

---
+++ src/main/java/io/github/ctalb/pokecolormatcher/PokeColorMatcherApplication.java
@@ -0,0 +1,13 @@
+package io.github.ctalb.pokecolormatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeColorMatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeColorMatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ctalb/pokecolormatcher/PokeColorMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ctalb.pokecolormatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeColorMatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
