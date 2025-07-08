e5f7eec78f57893e6b58aedea490abb3623a14ce
ctalb39 <ctalb39@wgu.edu>
2025-07-08 17:58:09 -0400

    Working on ColorMath rgbToXyz() finished and tests written

--- src/main/java/io/github/ctalb/pokecolormatcher/util/ColorMath.java
+++ src/main/java/io/github/ctalb/pokecolormatcher/util/ColorMath.java
@@ -21,9 +21,6 @@ public class ColorMath {
         double red = (double)rgb[0] / 255;
         double green = (double)rgb[1] / 255;
         double blue = (double)rgb[2] / 255;
-        System.out.println("Red: " + red);
-        System.out.println("Green: " + green);
-        System.out.println("Blue: " + blue);
 
         if (red > 0.04045) {
             red = Math.pow((red + 0.055) / 1.055, 2.4);
@@ -40,9 +37,6 @@ public class ColorMath {
         } else {
             blue = blue / 12.92;
         }
-        System.out.println("Red: " + red);
-        System.out.println("Green: " + green);
-        System.out.println("Blue: " + blue);
 
         red *= 100;
         green *= 100;
@@ -51,10 +45,6 @@ public class ColorMath {
         double x = (red * 0.4124) + (green * 0.3576) + (blue * 0.1805);
         double y = (red * 0.2126) + (green * 0.7152) + (blue * 0.0722);
         double z = (red * 0.0193) + (green * 0.1192) + (blue * 0.9505);
-
-        System.out.println("X: " + x);
-        System.out.println("Y: " + y);
-        System.out.println("Z: " + z);
 
         return new double[]{x, y, z};
     }
--- src/test/java/io/github/ctalb/pokecolormatcher/util/ColorMathTest.java
+++ src/test/java/io/github/ctalb/pokecolormatcher/util/ColorMathTest.java
@@ -10,14 +10,22 @@ class ColorMathTest {
         assertArrayEquals(expected, ColorMath.hexToRgb(hex));
     }
 
+    private void assertInvalidHex(String hex) {
+        assertThrows(IllegalArgumentException.class, () -> ColorMath.hexToRgb(hex));
+    }
+
+    private void assertRgbToXyz(int[] rgb, double[] expected, double delta) {
+        assertArrayEquals(expected, ColorMath.rgbToXyz(rgb), delta);
+    }
+
     @Test
     void hexToRgbWithHash() {
-        assertHexToRgb("#000000", new int[] {0, 0, 0});
-        assertHexToRgb("#ffffff", new int[] {255, 255, 255});
-        assertHexToRgb("#FF0000", new int[] {255, 0, 0});
-        assertHexToRgb("#00ff00", new int[] {0, 255, 0});
-        assertHexToRgb("#0000FF", new int[] {0, 0, 255});
-        assertHexToRgb("#f1adff", new int[] {241, 173, 255});
+        assertHexToRgb("#000000", new int[] {0, 0, 0}); // black
+        assertHexToRgb("#ffffff", new int[] {255, 255, 255}); // white
+        assertHexToRgb("#FF0000", new int[] {255, 0, 0}); // red
+        assertHexToRgb("#00ff00", new int[] {0, 255, 0}); // green
+        assertHexToRgb("#0000FF", new int[] {0, 0, 255}); // blue
+        assertHexToRgb("#f1adff", new int[] {241, 173, 255}); // lilac
     }
 
     @Test
@@ -31,9 +39,20 @@ class ColorMathTest {
     }
 
     @Test
+    void hexToRgbInvalidHex() {
+        assertInvalidHex("#123");
+        assertInvalidHex("#GGGGGG");
+        assertInvalidHex("#FFFFF");
+    }
+
+    @Test
     void rgbToXyzTest() {
-        int[] test = new int[] {184, 61, 186};
-        ColorMath.rgbToXyz(test);
+        assertRgbToXyz(new int[] {0, 0, 0}, new double[] {0.0000, 0.0000, 0.0000}, 0.001);
+        assertRgbToXyz(new int[] {255, 255, 255}, new double[] {95.0500, 100.0000, 108.9000}, 0.001);
+        assertRgbToXyz(new int[] {255, 0, 0}, new double[] {41.2400, 21.2600, 1.9300}, 0.001);
+        assertRgbToXyz(new int[] {0, 255, 0}, new double[] {35.7600, 71.5200, 11.9200}, 0.001);
+        assertRgbToXyz(new int[] {0, 0, 255}, new double[] {18.0500, 7.2200, 95.0500}, 0.001);
+        assertRgbToXyz(new int[] {241, 173, 255}, new double[] {69.2692, 55.8079, 101.7289}, 0.001);
     }
 
 }
