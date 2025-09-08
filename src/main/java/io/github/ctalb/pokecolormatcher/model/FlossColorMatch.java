9da844e5a19c04b8f4f8aeafd6422fdf20a4e687
ctalb39 <ctalb39@wgu.edu>
2025-09-08 15:48:31 -0400

    Override hashCode() for FlossColorMatch

--- src/main/java/io/github/ctalb/pokecolormatcher/model/FlossColorMatch.java
+++ src/main/java/io/github/ctalb/pokecolormatcher/model/FlossColorMatch.java
@@ -1,6 +1,7 @@
 package io.github.ctalb.pokecolormatcher.model;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public record FlossColorMatch(int [] extractedColor, DmcFloss match) {
 
@@ -10,5 +11,12 @@ public record FlossColorMatch(int [] extractedColor, DmcFloss match) {
         if (o == null || getClass() != o.getClass()) return false;
         FlossColorMatch that = (FlossColorMatch) o;
         return Arrays.equals(extractedColor, that.extractedColor) && match.equals(that.match);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(match);
+        result = 31 * result + Arrays.hashCode(extractedColor);
+        return result;
     }
 }
