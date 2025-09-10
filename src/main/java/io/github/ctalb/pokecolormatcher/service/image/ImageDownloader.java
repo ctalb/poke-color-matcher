eb0dbc0e9702ca45aba44b4675b8909044c278a0
ctalb39 <ctalb39@wgu.edu>
2025-09-10 18:04:25 -0400

    Add image processing service class shells

---
+++ src/main/java/io/github/ctalb/pokecolormatcher/service/image/ImageDownloader.java
@@ -0,0 +1,7 @@
+package io.github.ctalb.pokecolormatcher.service.image;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageDownloader {
+}
---
+++ src/main/java/io/github/ctalb/pokecolormatcher/service/image/ImageSaver.java
@@ -0,0 +1,9 @@
+package io.github.ctalb.pokecolormatcher.service.image;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageSaver {
+
+
+}
