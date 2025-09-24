ecc35f8262005df836a0d174cd8d797c480a50a5
ctalb39 <ctalb39@wgu.edu>
2025-09-24 17:59:46 -0400

    Add test in ImageDownloaderTest for comparing byte arrays

--- src/test/java/io/github/ctalb/pokecolormatcher/service/image/ImageDownloaderTest.java
+++ src/test/java/io/github/ctalb/pokecolormatcher/service/image/ImageDownloaderTest.java
@@ -1,6 +1,8 @@
 package io.github.ctalb.pokecolormatcher.service.image;
 
+import okhttp3.mockwebserver.MockResponse;
 import org.junit.jupiter.api.Test;
+import okhttp3.mockwebserver.MockWebServer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +15,24 @@ class ImageDownloaderTest {
 
         assertNotNull(actual);
         assertTrue(actual.length > 0);
+    }
+
+    @Test
+    void givenFakeImage_whenDownloadImage_thenReturnCorrectBytes() throws Exception {
+        MockWebServer server = new MockWebServer();
+
+        byte [] expected = {1, 2, 3, 4, 5};
+
+        server.enqueue(new MockResponse()
+                .setBody(new okio.Buffer().write(expected))
+                .addHeader("Content-Type", "image/png"));
+
+        server.start();
+        String url = server.url("/test.png").toString();
+
+        ImageDownloader imageDownloader = new ImageDownloader();
+        byte[] actual = imageDownloader.downloadImage(url);
+
+        assertArrayEquals(expected, actual);
     }
 }
