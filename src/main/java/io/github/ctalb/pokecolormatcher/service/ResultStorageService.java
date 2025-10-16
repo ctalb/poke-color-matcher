ed9dc9a56110409f3261a0099662493ec14eeb35
ctalb39 <ctalb39@wgu.edu>
2025-10-16 16:09:22 -0400

    Add saveResult() to ResultStorageService

--- src/main/java/io/github/ctalb/pokecolormatcher/service/ResultStorageService.java
+++ src/main/java/io/github/ctalb/pokecolormatcher/service/ResultStorageService.java
@@ -1,9 +1,32 @@
 package io.github.ctalb.pokecolormatcher.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.ctalb.pokecolormatcher.model.PokemonMatchResult;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class ResultStorageService {
 
+    private final ObjectMapper objectMapper;
 
+    public ResultStorageService(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Value("${results.folder}")
+    private String destinationFolder;
+
+    public void saveResult(PokemonMatchResult result, String pokemonName) throws IOException {
+
+        String fileName =  pokemonName + "_default_result.json";
+        Path directory = Paths.get(destinationFolder);
+        Path filePath = directory.resolve(fileName);
+        objectMapper.writeValue(filePath.toFile(), result);
+
+    }
 }
