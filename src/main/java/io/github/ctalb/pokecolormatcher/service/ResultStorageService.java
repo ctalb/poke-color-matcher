b5a67559e13052effed72897b57519a996331f52
ctalb39 <ctalb39@wgu.edu>
2025-10-21 14:38:18 -0400

    Add readResult() to ResultStorageService

--- src/main/java/io/github/ctalb/pokecolormatcher/service/ResultStorageService.java
+++ src/main/java/io/github/ctalb/pokecolormatcher/service/ResultStorageService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -27,5 +28,19 @@ public class ResultStorageService {
         Path filePath = directory.resolve(fileName);
         objectMapper.writeValue(filePath.toFile(), result);
 
+    }
+
+    public PokemonMatchResult readResult(String pokemonName) throws IOException {
+
+        String fileName =  pokemonName + "_default_result.json";
+        Path directory = Paths.get(folder);
+        Path filePath = directory.resolve(fileName);
+
+        if (Files.exists(filePath)) {
+
+            return objectMapper.readValue(filePath.toFile(), PokemonMatchResult.class);
+        }
+
+        return null;
     }
 }
