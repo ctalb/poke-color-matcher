582649ab733723b5e07fe6f4d4062726c0af51b2
ctalb39 <ctalb39@wgu.edu>
2025-11-10 14:06:57 -0500

    Add folder configuration to ImageService and add logic to handle the existing image check

--- src/main/java/io/github/ctalb/pokecolormatcher/service/image/ImageService.java
+++ src/main/java/io/github/ctalb/pokecolormatcher/service/image/ImageService.java
@@ -1,20 +1,33 @@
 package io.github.ctalb.pokecolormatcher.service.image;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Service
 public class ImageService {
     private final ImageDownloader imageDownloader;
     private final ImageSaver imageSaver;
+    private final String folder;
 
-    public ImageService(ImageDownloader imageDownloader, ImageSaver imageSaver) {
+    public ImageService(ImageDownloader imageDownloader, ImageSaver imageSaver,
+                        @Value("${images.folder}") String folder) {
         this.imageDownloader = imageDownloader;
         this.imageSaver = imageSaver;
+        this.folder = folder;
     }
 
     public Path getSavedImage(String pokemonName) {
+
+        String imageName = pokemonName + "_default.png";
+        Path directory = Path.of(folder);
+        Path imagePath = directory.resolve(imageName);
+        if (Files.exists(imagePath)) {
+            return imagePath;
+        }
+
         return null;
     }
 
