742a6a106e568e73300dca4cca482d1224aa208e
ctalb39 <ctalb39@wgu.edu>
2026-05-12 17:29:08 -0400

    Remove `PokemonSearchBar` and `MatchButton` components from `PokemonSelector`. Starting over with new knowledge of React

--- frontend/src/components/PokemonSelector.tsx
+++ frontend/src/components/PokemonSelector.tsx
@@ -1,30 +1,5 @@
-function PokemonSearchBar() {
-    return (
-        <form>
-            <input type="text" placeholder={"Enter Pokemon"}></input>
-        </form>
-    );
-}
 
-function MatchButton() {
-    function handleClick() {
-        console.log("matched");
-    }
-    return (
-        <button
-        onClick={handleClick}>
-            Get match
-        </button>
-    );
-}
 
 export default function PokemonSelector() {
 
-    return (
-        <>
-            <PokemonSearchBar />
-            <MatchButton />
-        </>
-
-    );
 }
