aa7a9145bda703ff46760414870043979e76ae0c
ctalb39 <ctalb39@wgu.edu>
2026-05-13 15:32:36 -0400

    Rebuild `PokemonSelector` with state management and basic input handling functionality

--- frontend/src/components/PokemonSelector.tsx
+++ frontend/src/components/PokemonSelector.tsx
@@ -1,5 +1,22 @@
-
+import {useState} from "react";
 
 export default function PokemonSelector() {
+    const [pokemon, setPokemon]= useState("");
 
+    function handleMatch() {
+        console.log(pokemon);
+    }
+
+    return (
+        <>
+            <input
+                type="text"
+                value={pokemon}
+                onChange={(e) => setPokemon(e.target.value)}
+            />
+            <button onClick={handleMatch}>
+                Match
+            </button>
+        </>
+    );
 }
