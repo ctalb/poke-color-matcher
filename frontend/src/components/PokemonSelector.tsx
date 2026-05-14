7eb130d74045226d84890ef560daef422522fac4
ctalb39 <ctalb39@wgu.edu>
2026-05-14 15:23:21 -0400

    Implement `handleMatch` with async fetch call and error handling in `PokemonSelector`

--- frontend/src/components/PokemonSelector.tsx
+++ frontend/src/components/PokemonSelector.tsx
@@ -3,8 +3,24 @@ import {useState} from "react";
 export default function PokemonSelector() {
     const [pokemon, setPokemon]= useState("");
 
-    function handleMatch() {
-        console.log(pokemon);
+    async function handleMatch() {
+        const url="http://localhost:8080/api/pokemon/pikachu/match";
+
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error(error.message);
+            } else {
+                console.error("Unknown error", error);
+            }
+        }
     }
 
     return (
