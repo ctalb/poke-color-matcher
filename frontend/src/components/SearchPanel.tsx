522b94ff2b1f5bbc246b563cb68a830e975c7c23
ctalb39 <ctalb39@wgu.edu>
2026-05-26 13:54:42 -0400

    Enhance `SearchPanel` input styling with Bootstrap classes for better UI consistency.

--- frontend/src/components/SearchPanel.tsx
+++ frontend/src/components/SearchPanel.tsx
@@ -13,14 +13,17 @@ export default function SearchPanel({pokemon, setPokemon, handleMatch}: searchPr
             <div className="card-body">
 
                 <p className="card-text">Enter a Pokémon's name to get a list of matching DMC flosses!</p>
-                <input
-                    type="text"
-                    value={pokemon}
-                    onChange={(e) => setPokemon(e.target.value)}
-                />
-                <button onClick={handleMatch}>
-                    Match
-                </button>
+                <div className="input-group mb-3">
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={pokemon}
+                        onChange={(e) => setPokemon(e.target.value)}
+                    />
+                    <button className="btn btn-primary" onClick={handleMatch}>
+                        Match
+                    </button>
+                </div>
             </div>
         </div>
     );
